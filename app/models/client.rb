c95994a53c3685253006acd14b3dd9730de80b4e
FabricioSobrinho <fabriciomails3@gmail.com>
2023-10-21 22:44:21 -0400

    Feat: added the created balance method

--- app/models/client.rb
+++ app/models/client.rb
@@ -14,6 +14,8 @@ class Client < ActiveRecord::Base
   before_create :generate_acc_number
   before_validation :set_uid
 
+  after_create :create_default_balance
+
   private
   def generate_acc_number
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -22,6 +24,10 @@ class Client < ActiveRecord::Base
     while self.acc_number.length < 8
       self.acc_number += numbers.sample.to_s
     end
+  end
+
+  def create_default_balance
+    Balance.create(user_balance: 0, client_id: self.id)
   end
 
   def set_uid
