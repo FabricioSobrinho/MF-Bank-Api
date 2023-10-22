d0ffc32bb99e27af7eeaea4ba2c6dc553f781ae8
FabricioSobrinho <fabriciomails3@gmail.com>
2023-10-21 22:40:32 -0400

    Feat: added the withdraw route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  resources :balances
   resources :account_movements
   mount_devise_token_auth_for 'Client', at: 'auth'
+
+  post 'withdraw' => 'balances#withdraw'
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
