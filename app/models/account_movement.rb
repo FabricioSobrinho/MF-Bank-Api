86bb9c516375183867dbbdccb0e7006c5cb8bbef
FabricioSobrinho <fabriciomails3@gmail.com>
2023-10-23 14:15:59 -0400

    Feat: actualized the acc movements model to accept clients ids

--- app/models/account_movement.rb
+++ app/models/account_movement.rb
@@ -1,4 +1,7 @@
 class AccountMovement < ApplicationRecord
   validates :montant, presence: true
   validates :movement_type, presence: true
+  validates :client_id, presence: true
+
+  belongs_to :client
 end
