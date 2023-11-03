08a20ae422934a1a46449bdea9749bc9f675924b
FabricioSobrinho <fabriciomails3@gmail.com>
2023-11-02 20:36:25 -0400

    Feat: added the 'target' identifier for account movements serializer

--- app/serializers/account_movement_serializer.rb
+++ app/serializers/account_movement_serializer.rb
@@ -1,3 +1,3 @@
 class AccountMovementSerializer < ActiveModel::Serializer
-  attributes :id, :montant, :sender, :movement_type, :created_at
+  attributes :id, :montant, :sender, :movement_type, :created_at, :target
 end
