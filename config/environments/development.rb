0808eb09115f6cf4e27731cc3df2b020a77ce0c1
FabricioSobrinho <fabriciomails3@gmail.com>
2023-12-03 17:21:59 -0400

    Bug fix: fix the wrong host for account confirmation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -86,5 +86,5 @@ Rails.application.configure do
     read_timeout:    5 }
 
   # Devise
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
