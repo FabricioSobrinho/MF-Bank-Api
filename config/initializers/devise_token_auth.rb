54f98644b54e7abec9a0b564eebf050845428181
FabricioSobrinho <fabriciomails3@gmail.com>
2023-12-03 17:11:38 -0400

    Test: testing the redirects

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -72,7 +72,7 @@ DeviseTokenAuth.setup do |config|
   # By default this value is expected to be sent by the client so that the API knows
   # where to redirect users after successful email confirmation. If this param is set,
   # the API will redirect to this value when no value is provided by the client.
-  config.default_confirm_success_url =  'https://mf-bank-fabriciosobrinho.vercel.app/login'
+  config.default_confirm_success_url = 'https://mf-bank-fabriciosobrinho.vercel.app/login'
 
   # By default this value is expected to be sent by the client so that the API knows
   # where to redirect users after successful password resets. If this param is set,
