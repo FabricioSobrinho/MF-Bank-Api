e7e121ed27f88d863d35bc6505b9af091ab9d2c1
FabricioSobrinho <fabriciomails3@gmail.com>
2023-12-04 10:13:44 -0400

    Chore: Correct the CORS origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://mf-bank.vercel.app/"
+    origins "https://mf-bank.vercel.app"
 
     resource "*",
       headers: :any,
